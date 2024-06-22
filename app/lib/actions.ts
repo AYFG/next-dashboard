7ef4cf034dd33113906338b1bc2c335fb343e25f
AYFG <sek82468246@gmail.com>
2024-06-22 15:43:04 +0900

    create invoices

---
+++ app/lib/actions.ts
@@ -0,0 +1,33 @@
+'use server';
+import { sql } from '@vercel/postgres';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
+// zod는 객체인 스키마를 정의한다 이 스키마는 데이터베이스에 저장하기 전에 유효성을 검사한다
+import { z } from 'zod';
+
+const FormSchema = z.object({
+  id: z.string(),
+  customerId: z.string(),
+  amount: z.coerce.number(),
+  status: z.enum(['pending', 'paid']),
+  date: z.string(),
+});
+
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
+
+export async function createInvoice(formData: FormData) {
+  const { customerId, amount, status } = CreateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+  const amountInCents = amount * 100;
+  const date = new Date().toISOString().split('T')[0];
+
+  await sql`
+    INSERT INTO invoices (customer_id, amount, status, date)
+    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})`;
+
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
+}
